39798ef51cf3d74c2c818366bec9528d3894ba4e
zlobste <nkrayniuk@gmail.com>
2021-06-29 19:23:50 +0300

    Implement a feature to search blocks by date

--- client/stats.go
+++ client/stats.go
@@ -2,19 +2,25 @@ package rpc
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/cosmos/cosmos-sdk/client"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/rest"
+	tendermint "github.com/tendermint/tendermint/types"
 	"net/http"
 	"time"
+)
+
+const (
+	MaxBlocksPerPage = 100
+
+	AllBlocksQuery = "block.height > 0"
+	OrderByAsc     = "asc"
 )
 
 type BlockAverageBlockSizeRequest struct {
 	StartDate time.Time `json:"start_time"`
 	EndDate   time.Time `json:"end_time"`
-}
-
-type BlockAverageBlockSizeResponse struct {
-	Result []AverageBlockSize `json:"average_block_size"`
 }
 
 type AverageBlockSize struct {
@@ -24,77 +30,82 @@ type AverageBlockSize struct {
 
 func GetAvgBlockSize(clientCtx client.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		node, err := clientCtx.GetNode()
+		var request BlockAverageBlockSizeRequest
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		var averageBlockSize []AverageBlockSize
+
+		blocksByDates, err := GetBlocksByDates(clientCtx, request.StartDate, request.EndDate)
 		if err != nil {
 			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 
-		req := BlockAverageBlockSizeRequest{
-			StartDate: time.Date(2021, 2, 1, 0, 0, 0, 0, time.UTC),
-			EndDate:   time.Now().UTC(),
-		}
-
-		info, err := node.ABCIInfo(context.Background())
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		height := int(info.Response.LastBlockHeight)
-		maxPerPage := 100
-		pages := height / maxPerPage
-		if pages*maxPerPage < height {
-			pages += 1
-		}
-
-		query := "block.height > 0"
-
-		var res []AverageBlockSize
-		var blocksCount, commonSize uint64
-		year, month, day := 0, time.Month(0), 0
-
-		for i := 1; i <= pages; i++ {
-			pageNumber := i
-			result, err := node.BlockSearch(context.Background(), query, &pageNumber, &maxPerPage, "asc")
-			if err != nil {
-				rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-				return
+		for key, value := range blocksByDates {
+			commonSize := 0
+			for _, block := range value {
+				commonSize += block.Size()
 			}
-
-			for j, r := range result.Blocks {
-				blockTime := r.Block.Header.Time.UTC()
-				if blockTime.After(req.StartDate) && blockTime.Before(req.EndDate) {
-					y, m, d := blockTime.Date()
-
-					isLastPage := j == len(result.Blocks)-1 && i == pages
-					if y != year || m != month || d != day || isLastPage {
-						if blocksCount != 0 || isLastPage {
-							averageSize := commonSize / blocksCount
-							date := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
-							bs := AverageBlockSize{
-								AverageSize: averageSize,
-								Date:        date,
-							}
-							res = append(res, bs)
-
-							commonSize = 0
-							blocksCount = 0
-						}
-
-						year, month, day = y, m, d
-					}
-
-					commonSize += uint64(r.Block.Size())
-					blocksCount++
-				}
-			}
+			averageSize := commonSize / len(value)
+			averageBlockSize = append(averageBlockSize, AverageBlockSize{
+				Date:        key,
+				AverageSize: uint64(averageSize),
+			})
 		}
 
-		ro := BlockAverageBlockSizeResponse{
-			Result: res,
-		}
-
-		rest.PostProcessResponseBare(w, clientCtx, ro)
+		rest.PostProcessResponseBare(w, clientCtx, averageBlockSize)
 	}
+}
+
+func GetBlocksByDates(clientCtx client.Context, startDate, endDate time.Time) (map[time.Time][]*tendermint.Block, error) {
+	node, err := clientCtx.GetNode()
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "failed to get node")
+	}
+
+	info, err := node.ABCIInfo(context.Background())
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "failed to get ABCI info")
+	}
+
+	maxBlocksPerPage := MaxBlocksPerPage
+	height := int(info.Response.LastBlockHeight)
+	pages := height / maxBlocksPerPage
+	if pages*maxBlocksPerPage < height {
+		pages++
+	}
+
+	blocksPerDay := make(map[time.Time][]*tendermint.Block, 0)
+
+	for i := 1; i <= pages; i++ {
+		pageNumber := i
+		result, err := node.BlockSearch(
+			context.Background(),
+			AllBlocksQuery,
+			&pageNumber,
+			&maxBlocksPerPage,
+			OrderByAsc,
+		)
+		if err != nil {
+			return nil, sdkerrors.Wrap(err, "failed to get find the blocks")
+		}
+
+		for _, r := range result.Blocks {
+			blockDate := TimeToUTCDate(r.Block.Header.Time)
+			if blockDate.After(startDate) && blockDate.Before(endDate) {
+				blocksPerDay[blockDate] = append(blocksPerDay[blockDate], r.Block)
+			}
+		}
+	}
+
+	return blocksPerDay, nil
+}
+
+func TimeToUTCDate(t time.Time) time.Time {
+	year, month, day := t.UTC().Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 }
